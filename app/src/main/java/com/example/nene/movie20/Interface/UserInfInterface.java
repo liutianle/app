d83468e4347c576ea77211becc5f3805fe6fe1ed
zwp11 <1774735953@qq.com>
2018-05-15 15:00:22 +0800

    login activity and userinf  by zwp

--- app/src/main/java/com/example/nene/movie20/Interface/UserInfInterface.java
+++ app/src/main/java/com/example/nene/movie20/Interface/UserInfInterface.java
@@ -1,16 +1,21 @@
 package com.example.nene.movie20.Interface;
 
 
+import com.example.nene.movie20.models.User;
 import com.example.nene.movie20.models.UserInformation;
+
+import java.lang.ref.SoftReference;
 
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Path;
 
 /**
  * Created by sam- on 2018/4/24.
  */
 public interface UserInfInterface {
-    @GET("read/")
-    Call<UserInformation> getinformation(@Field("id") String id);
+    @GET("read/{id}/")
+    Call<User> getinformation(@Header("Authorization") String token, @Path("id") String id);
 }
--- app/src/main/java/com/example/nene/movie20/utils/UserInfUtils.java
+++ app/src/main/java/com/example/nene/movie20/utils/UserInfUtils.java
@@ -1,5 +1,27 @@
 package com.example.nene.movie20.utils;
 
-public class UserInfUtils {
+import android.content.SharedPreferences;
 
+import com.example.nene.movie20.Interface.UserInfInterface;
+import com.example.nene.movie20.activity.SplashActivity;
+import com.example.nene.movie20.models.User;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import static com.example.nene.movie20.models.Constant.BaseUrl;
+
+public class UserInfUtils {
+    public static void getUserInf() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(Constant.BaseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        final UserInfInterface userInfInterface = retrofit.create(UserInfInterface.class);
+        System.out.println(SplashActivity.Token);
+        final Call<User> call = userInfInterface.getinformation("JWT "+ SplashActivity.Token, "1");
+
+    }
 }
