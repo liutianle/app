2fefcf9e79d629418fbef280974e529d776f32f0
Liutianle <2712118128@qq.com>
2018-05-24 15:27:18 +0800

    setting

--- app/src/main/java/com/example/nene/movie20/activity/AdminSettingActivity.java
+++ app/src/main/java/com/example/nene/movie20/activity/AdminSettingActivity.java
@@ -1,22 +1,59 @@
 package com.example.nene.movie20.activity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.BottomSheetDialog;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
+import com.bigkoo.pickerview.builder.OptionsPickerBuilder;
+import com.bigkoo.pickerview.builder.TimePickerBuilder;
+import com.bigkoo.pickerview.listener.CustomListener;
+import com.bigkoo.pickerview.listener.OnOptionsSelectListener;
+import com.bigkoo.pickerview.listener.OnTimeSelectChangeListener;
+import com.bigkoo.pickerview.listener.OnTimeSelectListener;
+import com.bigkoo.pickerview.view.OptionsPickerView;
+import com.bigkoo.pickerview.view.TimePickerView;
 import com.example.nene.movie20.R;
+import com.example.nene.movie20.data.CardBean;
+import com.example.nene.movie20.data.CommentBean;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class AdminSettingActivity extends AppCompatActivity {
     private Intent intent;
     private LinearLayout exit_login;
     private ImageView back;
+    private TimePickerView pvTime;
+    private TextView admin_birth;
+    private TextView admin_adddress;
+    private TextView admin_sex;
+    private TextView admin_nickname;
+    private OptionsPickerView pvOptions;
+    private ArrayList<String> sex;
+    private ArrayList<CardBean> cardItem = new ArrayList<>();
+    private BottomSheetDialog dialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +64,202 @@ public class AdminSettingActivity extends AppCompatActivity {
         toolbar.setTitle("");
         setSupportActionBar(toolbar);
 
+        //先加载数据
+        getCardData();
+
+        admin_birth = findViewById(R.id.admin_birth);
+        admin_birth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pvTime.show(v);
+            }
+        });
+
+        admin_adddress = findViewById(R.id.admin_address);
+        admin_adddress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog();
+            }
+        });
+
+        admin_nickname = findViewById(R.id.admin_nickname);
+        admin_nickname.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showNameDialog();
+            }
+        });
+
+
+        admin_sex = findViewById(R.id.admin_sex);
+        admin_sex.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pvOptions.show();
+            }
+        });
+
         initView();
+        initTime();
+        initSex();
+    }
+
+    private void showNameDialog() {
+        dialog = new BottomSheetDialog(this);
+        View commentView = LayoutInflater.from(this).inflate(R.layout.review_dialog, null);
+        final EditText text = (EditText) commentView.findViewById(R.id.dialog_comment_et);
+        final Button submit = (Button) commentView.findViewById(R.id.dialog_comment_bt);
+        dialog.setContentView(commentView);
+
+        View parent = (View) commentView.getParent();
+        BottomSheetBehavior<View> behavior = BottomSheetBehavior.from(parent);
+        commentView.measure(0, 0);
+        behavior.setPeekHeight(commentView.getMeasuredHeight());
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String address = text.getText().toString().trim();
+                admin_nickname.setText(address);
+            }
+        });
+
+        text.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                if (!TextUtils.isEmpty(s) && s.length() > 2) {
+                    submit.setBackgroundColor(Color.parseColor("#FFB568"));
+                } else {
+                    submit.setBackgroundColor(Color.parseColor("#D8D8D8"));
+                }
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        dialog.show();
+    }
+
+    private void showDialog() {
+        dialog = new BottomSheetDialog(this);
+        View commentView = LayoutInflater.from(this).inflate(R.layout.review_dialog, null);
+        final EditText text = (EditText) commentView.findViewById(R.id.dialog_comment_et);
+        final Button submit = (Button) commentView.findViewById(R.id.dialog_comment_bt);
+        dialog.setContentView(commentView);
+
+        View parent = (View) commentView.getParent();
+        BottomSheetBehavior<View> behavior = BottomSheetBehavior.from(parent);
+        commentView.measure(0, 0);
+        behavior.setPeekHeight(commentView.getMeasuredHeight());
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String address = text.getText().toString().trim();
+                admin_adddress.setText(address);
+            }
+        });
+
+        text.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                if (!TextUtils.isEmpty(s) && s.length() > 2) {
+                    submit.setBackgroundColor(Color.parseColor("#FFB568"));
+                } else {
+                    submit.setBackgroundColor(Color.parseColor("#D8D8D8"));
+                }
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        dialog.show();
+    }
+
+    private void initSex() {
+        /**
+         * @description
+         *
+         * 注意事项：
+         * 自定义布局中，id为 optionspicker 或者 timepicker 的布局以及其子控件必须要有，否则会报空指针。
+         * 具体可参考demo 里面的两个自定义layout布局。
+         */
+        pvOptions = new OptionsPickerBuilder(this, new OnOptionsSelectListener() {
+            @Override
+            public void onOptionsSelect(int options1, int option2, int options3, View v) {
+                //返回的分别是三个级别的选中位置
+                String tx = cardItem.get(options1).getPickerViewText();
+                admin_sex.setText(tx);
+            }
+        })
+                .setLayoutRes(R.layout.pickerview_custom_options, new CustomListener() {
+                    @Override
+                    public void customLayout(View v) {
+                        final TextView tvSubmit = (TextView) v.findViewById(R.id.tv_finish);
+
+                        ImageView ivCancel = (ImageView) v.findViewById(R.id.iv_cancel);
+                        tvSubmit.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                pvOptions.returnData();
+                                pvOptions.dismiss();
+                            }
+                        });
+
+                        ivCancel.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                pvOptions.dismiss();
+                            }
+                        });
+                    }
+                })
+                .isDialog(true)
+                .build();
+
+        pvOptions.setPicker(cardItem);//添加数据
+
+
+    }
+
+    public void initTime() {
+        pvTime = new TimePickerBuilder(this, new OnTimeSelectListener() {
+            @Override
+            public void onTimeSelect(Date date, View v) {
+                admin_birth.setText(getTime(date));
+                Log.i("pvTime", "onTimeSelect");
+            }
+
+        }).build();
+    }
+
+    public String getTime(Date date){
+        Log.d("getTime", "getTime: "+date.getTime());
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        return format.format(date);
+    }
+
+
+    private void getCardData() {
+        int i = 1;
+        cardItem.add(new CardBean(i, "男"));
+        cardItem.add(new CardBean(i, "女"));
+        cardItem.add(new CardBean(i, "保密"));
     }
 
     private void initView() {
@@ -51,3 +283,6 @@ public class AdminSettingActivity extends AppCompatActivity {
     }
 
 }
+
+
+
